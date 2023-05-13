3193961c8d87d50e3fd0aefe532a8a08695e1dad
pkn <PKN199@yandex.ru>
2023-05-13 16:34:24 +0300

    ADD:productDTO

--- JavaScript/ProductDTO.js
+++ JavaScript/ProductDTO.js
@@ -1,6 +1,52 @@
-const API = 'https://raw.githubusercontent.com/Astarog199/online-store-api/main/DATAbase_clothing-store/';
+
+const API = 'https://raw.githubusercontent.com/Astarog199/online-store-api/main/';
 
 
-const app= new Vue({
-    
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: "DATAbase_clothing-store/catalogDATA.json",
+    products:[],
+  },
+      methods: {
+    getJson(url){
+      return fetch(url)
+          .then(result => result.json())
+          .catch(error => {
+            console.log(error);
+          })
+    },
+    addProduct(good) {
+      console.log(good);
+    }
+  },
+  beforeCreate() {
+
+  },
+  created() {
+    this.getJson(`${API + this.catalogUrl}`)
+        .then(data => {
+          for(let el of data){
+            this.products.push(el);
+          }
+        });
+  },
+  beforeMount() {
+
+  },
+  mounted() {
+
+  },
+  beforeUpdate() {
+
+  },
+  updated() {
+
+  },
+  beforeDestroy() {
+
+  },
+  destroyed() {
+
+  }
 })
