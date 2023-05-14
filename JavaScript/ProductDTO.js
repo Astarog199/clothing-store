724ed9543e766de1cbe0ecc494672e0e6b83da16
pkn <PKN199@yandex.ru>
2023-05-14 16:08:09 +0300

    update:Cart

--- JavaScript/ProductDTO.js
+++ JavaScript/ProductDTO.js
@@ -1,12 +1,14 @@
-
 const API = 'https://raw.githubusercontent.com/Astarog199/online-store-api/main/';
-
 
 const app = new Vue({
   el: '#app',
   data: {
     catalogUrl: "DATAbase_clothing-store/catalogDATA.json",
+    basketUrl: "DATAbase_clothing-store/getBasket.json",
     products:[],
+    cartItems:[],
+    carttotal:0,
+    countGoods:0,
   },
       methods: {
     getJson(url){
@@ -18,7 +20,21 @@ const app = new Vue({
     },
     addProduct(good) {
       console.log(good);
-    }
+    },
+      filter(value) {
+    const regexp = new RegExp(value, 'i');
+    this.filtered = this.allProducts.filter(product => regexp.test(product.product_name));
+    this.allProducts.forEach(el => {
+      const block = document.querySelector(`.product-item[data-id="${el.id}"]`);
+      if (!this.filtered.includes(el)) {
+        block.classList.remove('show');
+        block.classList.add('invisible');
+      } else {
+        block.classList.remove('invisible');
+        block.classList.add('show');
+      }
+    })
+  }
   },
   beforeCreate() {
 
@@ -30,12 +46,20 @@ const app = new Vue({
             this.products.push(el);
           }
         });
+
   },
   beforeMount() {
 
   },
   mounted() {
-
+    this.getJson(`${API + this.basketUrl}`)
+    .then(data => {
+      for(let el of data.contents){
+        this.cartItems.push(el);
+      }
+     this.carttotal = data.amount;
+     this.countGoods = data.countGoods;
+    });
   },
   beforeUpdate() {
 
